8b2691debbce51c6706eab101dbc4b2cb2d780e2
Spencer Childress <samussiah@gmail.com>
2016-11-16 16:37:38 -0500

    Place code to draw links in a function.

--- build/interactiveSankey.js
+++ build/interactiveSankey.js
@@ -45,6 +45,98 @@ var interactiveSankey = function (webcharts) {
         var context = this;
     }
 
+    function drawLinks(chartObject, bars1, bars2, linkClass) {
+
+        //Merge adjacent bar groups by [ config.y.column ].
+        var linkData = [];
+        bars1.each(function (bar1, i1) {
+            bars2.each(function (bar2, i2) {
+                bar1.values.raw.forEach(function (id1) {
+                    bar2.values.raw.forEach(function (id2) {
+                        if (id1[chartObject.config.y.column] === id2[chartObject.config.y.column]) linkData.push({ id: id1[chartObject.config.y.column],
+                            split1: id1[chartObject.config.color_by],
+                            x1: id1[chartObject.config.x.column],
+                            split2: id2[chartObject.config.color_by],
+                            x2: id2[chartObject.config.x.column] });
+                    });
+                });
+            });
+        });
+
+        //Nest merged bar groups by their respective [ config.marks.color_by ] values.
+        var nestedLinkData = d3.nest().key(d => d.split1).key(d => d.split2).rollup(d => {
+            return { n: d.length, IDs: d.map(di => di.id) };
+        }).entries(linkData);
+        nestedLinkData.sort((a, b) => a.key > b.key ? 1 : -1);
+
+        //Flatten nested data array to one item per left bar [ config.marks.color_by ]
+        //value per right bar [ config.marks.color_by ] value.
+        var collapsedLinkData = [];
+        var offsetList = [];
+        nestedLinkData.forEach((d, i) => {
+            var cat1 = bars1.data()[0].values.x;
+            var bar1 = bars1.filter(dii => dii.key === d.key).data()[0].values;
+            var offset1 = 0;
+            d.values.sort((a, b) => a.key < b.key ? -1 : b.key < a.key ? 1 : 0);
+
+            d.values.forEach(di => {
+                var cat2 = bars2.data()[0].values.x;
+                var bar2 = bars2.filter(dii => dii.key === di.key).data()[0].values;
+                offsetList.push({ key: di.key, offset: 0 });
+                var offset2 = offsetList.filter(dii => dii.key === di.key)[0].offset;
+
+                collapsedLinkData.push({ split1: d.key,
+                    split2: di.key,
+
+                    x1: chartObject.x(cat1),
+                    x2: chartObject.x(cat2),
+
+                    y1: bar1.y,
+                    y2: bar2.y,
+
+                    start1: bar1.start,
+                    start2: bar2.start,
+
+                    stop1: bar1.start - bar1.y,
+                    stop2: bar2.start - bar2.y,
+
+                    y01: bar1.start - bar1.y + offset1,
+                    y11: bar1.start - bar1.y + di.values.n + offset1,
+
+                    y02: bar2.start - bar2.y + offset2,
+                    y12: bar2.start - bar2.y + di.values.n + offset2,
+
+                    n: di.values.n,
+                    height: chartObject.y(di.values.n),
+
+                    IDs: di.values.IDs });
+
+                offset1 += collapsedLinkData[collapsedLinkData.length - 1].n;
+                offsetList.filter(dii => dii.key === di.key)[0].offset += di.values.n;
+            });
+        });
+
+        //Draw links.
+        var pathDrawer = d3.svg.area().x(d => d.x).y0(d => d.y0).y1(d => d.y1);
+        collapsedLinkData.forEach((d, i) => {
+            var path = [{ split1: d.split1,
+                split2: d.split2,
+                n: d.n,
+                IDs: d.IDs,
+
+                x: d.x1 + chartObject.x.rangeBand(),
+                y0: chartObject.y(d.y01),
+                y1: chartObject.y(d.y11) }, { x: d.x2,
+                y0: chartObject.y(d.y02),
+                y1: chartObject.y(d.y12) }];
+            chartObject.svg.append('path').datum(path).attr({ 'd': pathDrawer,
+                'class': 'link ' + linkClass }).style({ 'fill': () => chartObject.colorScale(d.split1),
+                'fill-opacity': .5,
+                'stroke': () => chartObject.colorScale(d.split1),
+                'stroke-opacity': .5 });
+        });
+    }
+
     function onResize() {
         var context = this;
 
@@ -89,97 +181,7 @@ var interactiveSankey = function (webcharts) {
             if (i < nBarGroups) {
                 var barGroup1 = d3.select(this);
                 var barGroup2 = d3.select(barGroups[0][i + 1]);
-                var bars1 = barGroup1.selectAll('.bar');
-                var bars2 = barGroup2.selectAll('.bar');
-
-                //Merge adjacent bar groups by [ config.y.column ].
-                var linkData = [];
-                bars1.each(function (bar1, i1) {
-                    bars2.each(function (bar2, i2) {
-                        bar1.values.raw.forEach(function (id1) {
-                            bar2.values.raw.forEach(function (id2) {
-                                if (id1[context.config.y.column] === id2[context.config.y.column]) linkData.push({ id: id1[context.config.y.column],
-                                    split1: id1[context.config.color_by],
-                                    x1: id1[context.config.x.column],
-                                    split2: id2[context.config.color_by],
-                                    x2: id2[context.config.x.column] });
-                            });
-                        });
-                    });
-                });
-
-                //Nest merged bar groups by their respective [ config.marks.color_by ] values.
-                var nestedLinkData = d3.nest().key(d => d.split1).key(d => d.split2).rollup(d => {
-                    return { n: d.length, IDs: d.map(di => di.id) };
-                }).entries(linkData);
-                nestedLinkData.sort((a, b) => a.key > b.key ? 1 : -1);
-
-                //Flatten nested data array to one item per left bar [ config.marks.color_by ]
-                //value per right bar [ config.marks.color_by ] value.
-                var collapsedLinkData = [];
-                var offsetList = [];
-                nestedLinkData.forEach((d, i) => {
-                    var cat1 = barGroup1.data()[0].key;
-                    var bar1 = bars1.filter(dii => dii.key === d.key).data()[0].values;
-                    var offset1 = 0;
-                    d.values.sort((a, b) => a.key < b.key ? -1 : b.key < a.key ? 1 : 0);
-
-                    d.values.forEach(di => {
-                        var cat2 = barGroup2.data()[0].key;
-                        var bar2 = bars2.filter(dii => dii.key === di.key).data()[0].values;
-                        offsetList.push({ key: di.key, offset: 0 });
-                        var offset2 = offsetList.filter(dii => dii.key === di.key)[0].offset;
-
-                        collapsedLinkData.push({ split1: d.key,
-                            split2: di.key,
-
-                            x1: context.x(cat1),
-                            x2: context.x(cat2),
-
-                            y1: bar1.y,
-                            y2: bar2.y,
-
-                            start1: bar1.start,
-                            start2: bar2.start,
-
-                            stop1: bar1.start - bar1.y,
-                            stop2: bar2.start - bar2.y,
-
-                            y01: bar1.start - bar1.y + offset1,
-                            y11: bar1.start - bar1.y + di.values.n + offset1,
-
-                            y02: bar2.start - bar2.y + offset2,
-                            y12: bar2.start - bar2.y + di.values.n + offset2,
-
-                            n: di.values.n,
-                            height: context.y(di.values.n),
-
-                            IDs: di.values.IDs });
-
-                        offset1 += collapsedLinkData[collapsedLinkData.length - 1].n;
-                        offsetList.filter(dii => dii.key === di.key)[0].offset += di.values.n;
-                    });
-                });
-
-                //Draw links.
-                var pathDrawer = d3.svg.area().x(d => d.x).y0(d => d.y0).y1(d => d.y1);
-                collapsedLinkData.forEach((d, i) => {
-                    var path = [{ split1: d.split1,
-                        split2: d.split2,
-                        n: d.n,
-                        IDs: d.IDs,
-
-                        x: d.x1 + context.x.rangeBand(),
-                        y0: context.y(d.y01),
-                        y1: context.y(d.y11) }, { x: d.x2,
-                        y0: context.y(d.y02),
-                        y1: context.y(d.y12) }];
-                    context.svg.append('path').datum(path).attr({ 'd': pathDrawer,
-                        'class': 'link' }).style({ 'fill': () => context.colorScale(d.split1),
-                        'fill-opacity': .5,
-                        'stroke': () => context.colorScale(d.split1),
-                        'stroke-opacity': .5 });
-                });
+                drawLinks(context, barGroup1.selectAll('.bar'), barGroup2.selectAll('.bar'), 'defaultLink');
             }
 
             //Add event listeners and tooltips to links.
@@ -218,6 +220,7 @@ var interactiveSankey = function (webcharts) {
         bars.style('cursor', 'pointer').on('click', function (d) {
             //Reduce bar opacity.
             bars.style('fill-opacity', .25);
+            context.wrap.selectAll('.defaultLink').style('display', 'none');
             //Capture [ settings.id_col ] values represented by selected bar.
             var selectedIDs = d.values.raw.map(d => d[context.config.id_col]);
             //Filter raw data on selected [ settings.id_col ] values and nest by node and link.
@@ -225,21 +228,34 @@ var interactiveSankey = function (webcharts) {
             //Flatten nested data to one item per node per link.
             var selectedBarData = [];
             selectedData.forEach(d => {
-                d.values.forEach(di => {
-                    selectedBarData.push({ node: d.key,
-                        link: di.key,
-                        start: barData.filter(dii => dii.node === d.key && dii.link === di.key)[0].start,
-                        n: di.values });
-                });
+                d.values.forEach(di => selectedBarData.push({ key: di.key,
+                    values: { raw: context.raw_data.filter(dii => dii[context.config.node_col].toString() === d.key.toString() && dii[context.config.link_col].toString() === di.key.toString()),
+                        x: d.key,
+                        y: di.values,
+                        start: barData.filter(dii => dii.node === d.key && dii.link === di.key)[0].start } }));
             });
             //Draw bars.
             context.wrap.selectAll('.selectedIDs').remove();
             var selectedIDbars = context.svg.selectAll('rect.selectedIDs').data(selectedBarData).enter().append('rect').attr({ class: 'selectedIDs',
-                x: d => context.x(d.node),
-                y: d => context.y(d.start),
+                x: d => context.x(d.values.x),
+                y: d => context.y(d.values.start),
                 width: d3.select(this).attr('width'),
-                height: d => context.y(d.start - d.n) - context.y(d.start) }).style({ fill: d => context.colorScale(d.link),
-                stroke: d => context.colorScale(d.link) });
+                height: d => context.y(d.values.start - d.values.y) - context.y(d.values.start) }).style({ fill: d => context.colorScale(d.key),
+                stroke: d => context.colorScale(d.key) });
+            //Draw links.
+            var nodes = selectedData.map(d => d.key);
+            for (var i = 0; i < nodes.length; i++) {
+                if (i < nodes.length - 1) {
+                    drawLinks(context, selectedIDbars.filter(d => d.values.x === nodes[i]), selectedIDbars.filter(d => d.values.x === nodes[i + 1]), 'selectedLink');
+                }
+            }
+            //Add click event listener to selected bars.
+            selectedIDbars.style('cursor', 'pointer').on('click', function () {
+                bars.style('fill-opacity', 1);
+                selectedIDbars.remove();
+                context.wrap.selectAll('.selectedLink').remove();
+                context.wrap.selectAll('.defaultLink').style('display', '');
+            });
         });
     }
 
